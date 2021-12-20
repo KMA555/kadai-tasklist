2a9e40248a9e84024da0f1f47ebff6d49a005e84
keima.miyamoto <kmakma230@gmail.com>
2021-12-20 23:20:53 +0900

    3度目の修正

--- src/filters/EncodingFilter.java
+++ src/filters/EncodingFilter.java
@@ -20,14 +20,14 @@ public class EncodingFilter implements Filter {
      * Default constructor.
      */
     public EncodingFilter() {
-        // TODO Auto-generated constructor stub
+
     }
 
     /**
      * @see Filter#destroy()
      */
     public void destroy() {
-        // TODO Auto-generated method stub
+
     }
 
     /**
@@ -35,10 +35,9 @@ public class EncodingFilter implements Filter {
      */
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        // TODO Auto-generated method stub
-        // place your code here
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
 
-        // pass the request along the filter chain
         chain.doFilter(request, response);
     }
 
@@ -46,7 +45,7 @@ public class EncodingFilter implements Filter {
      * @see Filter#init(FilterConfig)
      */
     public void init(FilterConfig fConfig) throws ServletException {
-        // TODO Auto-generated method stub
+
     }
 
 }
